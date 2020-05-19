79af4ccde930d7918d42aeafbacc5d3c96d13d55
Yoshinori Shibahara <yoshinori.shibahara@gmail.com>
2020-05-19 18:47:39 +0900

    Initial Commit

---
+++ leaders_99_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  leaders_99_project
+//
+//  Created by Yoshinori Shibahara on 2020/05/19.
+//  Copyright © 2020 Yoshinori Shibahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
