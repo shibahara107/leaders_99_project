2980498444b01e4f8a12d87ec6c54572d3e197b5
Yoshinori Shibahara <yoshinori.shibahara@gmail.com>
2020-05-19 19:30:39 +0900

    First commit. Installed pods, got the app connected to Firebase. Running test codes, minor bugs couldn't be fixed.

--- leaders_99_project/ViewController.swift
+++ leaders_99_project/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var textView: UITextView!
+    @IBOutlet var nameInputField: UITextField!
+    @IBOutlet var messageInputField: UITextField!
+    
+    var databaseRefrence: DatabaseRefrence!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        databaseRefrence = Database.detabase().refrence()
+        
+        databaseRefrence.observe(.childAdded, with: { snapshot in
+            if let obj = snapshot.value as? [String: AnyObject], let name = obj["name"] as? String, let message = obj["message"] {
+            let currentText = self.textView.text
+            self.textView.text = (currentText ?? "") + "\n\(name) : \(message)"
+            }
+        })
     }
+    
+    @IBAction func tappedSendButton(_ sender: Any) {
+     view.endEditing(true)
 
-
+     if let name = nameInputField.text, let message = messageInputField.text {
+         let messageData = ["name": name, "message": message]
+        }
+    }
+    
 }
-
